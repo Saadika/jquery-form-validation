778b46763fd0fc43248f31058b8d494afa7884dc
Saadika <saadika.alard@gmail.com>
2018-07-23 06:11:12 +0200

    last changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,3 +104,17 @@ $('.save-button').click(function(e) {
 });
     
 
+// Form data collection
+$('.form').submit(function() {
+    // get all the inputs into an array.
+    var $inputs = $('.form :input');
+
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    var values = {};
+    $inputs.each(function() {
+        values[this.name] = $(this).val();
+    });
+
+});
+
